7cf236ecca408eef15a4bb9009f4147197ef24d4
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-02-12 09:19:41 +0000

    Add files via upload

--- Black_Hole_56.py
+++ Black_Hole_56.py
@@ -1,19 +1,19 @@
 import os
-pm = input("encrypt/decrypt")
+pm == input("encrypt/decrypt")
 if pm="encrypt":
     method = input("Method compress 1,2,3,4: ")
     if method=="1":
-         os.system("python Black_Hole_m1e.py")
+         os.system("python Black_Hole_m1.py")
     elif method=="2":
-        os.system("python Black_Hole_m2e.py")
+        os.system("python Black_Hole_m2.py")
         
     elif method=="3":
-        os.system("python Black_Hole_m3e.py")
+        os.system("python Black_Hole_m3.py")
         
     elif method=="4":
-        os.system("python Black_Hole_m4e.py")
+        os.system("python Black_Hole_m4.py")
   
-if pm="decrypt":
+if pm=="decrypt":
      
     os.system("password-7.py")
 
