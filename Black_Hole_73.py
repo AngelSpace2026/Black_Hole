08aadacfd9944d2c181f9e8975a015a2077389d5
Jurijus Pacalovas <j.pacalovas@mail.ru>
2025-03-12 10:24:56 +0000

    Add files via upload

---
+++ Black_Hole_73.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Jurijus Pacalovas\Documents\Black_Hole_1-72\Black_Hole-main\Black_Hole_72.py 
+Created by Jurijus Pacalovas.
+Enter mode (1 for compress, 2 for extract): 1
+Enter input file name to compress: Personal Statement Jurijus Pacalovas.docx
+Improved compression: 10457 bytes (chunk size: 7, positions: [21, 95, 200, 215, 255, 278, 285, 403, 416, 463, 532, 534, 591, 627, 642, 660, 691, 695, 726, 737, 791, 801, 816, 891, 938, 1101, 1108, 1121, 1122, 1148, 1163, 1243, 1287, 1310, 1374, 1444, 1456, 1481, 1524, 1563, 1581, 1598, 1621, 1645, 1661, 1701, 1771, 1850])
+Improved compression: 10423 bytes (chunk size: 101, positions: [6, 12, 18, 55, 58, 67, 71, 72, 89, 91, 108, 114, 121])
+Improved compression: 10351 bytes (chunk size: 74, positions: [19, 43, 47, 132, 150])
+Improved compression: 10342 bytes (chunk size: 64, positions: [2, 47, 84, 164])
+Improved compression: 10333 bytes (chunk size: 194, positions: [])
+Improved compression: 10331 bytes (chunk size: 161, positions: [])
+Improved compression: 10330 bytes (chunk size: 160, positions: [])
